4bc246d2c207de9a4e784327b643058f1bbd1386
szianiOne <113189675+szianiOne@users.noreply.github.com>
2024-02-20 07:32:17 +0000

    Copilot third commit

--- comments.js
+++ comments.js
@@ -1,39 +1,42 @@
 // Create a web server for comments
+// To run this file, execute the following command in the terminal:
+// node comments.js
+// Open a browser and type the following URL:
+// http://localhost:3000/
 
-const express = require('express');
-const bodyParser = require('body-parser');
-const app = express();
-const port = 3000;
+var http = require('http');
+var url = require('url');
+var fs = require('fs');
+var qs = require('querystring');
 
-// Use body-parser middleware to parse the request body
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
-
-// Create an array to store the comments
-let comments = [];
-
-// Create a route to get all the comments
-app.get('/comments', (req, res) => {
-  res.json(comments);
-});
-
-// Create a route to add a new comment
-app.post('/comments', (req, res) => {
-  const newComment = req.body;
-  comments.push(newComment);
-  res.json(newComment);
-});
-
-// Start the server
-app.listen(port, () => {
-  console.log(`Server is running at http://localhost:${port}`);
-});
-
-// Run the server and send a POST request to add a new comment
-// Use curl to send a POST request
-// $ curl -X POST -H "Content-Type: application/json" -d '{"name": "John", "message": "Hello, World!"}' http://localhost:3000/comments
-// {"name":"John","message":"Hello, World!"}
-
-// Use curl to send a GET request
-// $ curl http://localhost:3000/comments
-// [{"name":"John","message":"Hello, World!"}]
+http.createServer(function (req, res) {
+  var pathname = url.parse(req.url).pathname;
+  console.log('Request for ' + pathname + ' received.');
+  if (pathname === '/comments') {
+    if (req.method === 'GET') {
+      fs.readFile('comments.json', function(err, data) {
+        res.writeHead(200, { 'Content-Type': 'application/json' });
+        res.write(data);
+        res.end();
+      });
+    } else if (req.method === 'POST') {
+      var body = '';
+      req.on('data', function(chunk) {
+        body += chunk.toString();
+      });
+      req.on('end', function() {
+        var comment = qs.parse(body);
+        fs.readFile('comments.json', function(err, data) {
+          var comments = JSON.parse(data);
+          comments.push(comment);
+          fs.writeFile('comments.json', JSON.stringify(comments, null, 2), function(err) {
+            res.writeHead(200, { 'Content-Type': 'application/json' });
+            res.write(JSON.stringify(comments));
+            res.end();
+          });
+        });
+      });
+    }
+  }
+}
+).listen(3000);
